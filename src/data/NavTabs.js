dbe0a6245f06c95d21ae514c2494cbd480128f2d
Siddharth-Karanchery <webdev.siddharth@gmail.com>
2023-06-12 22:39:13 +0530

    Added NAvbar with basic routing

---
+++ src/data/NavTabs.js
@@ -0,0 +1,14 @@
+export const NavTabs = [
+  {
+    tabName: "Home",
+    path: "/",
+  },
+  {
+    tabName: "Tab 1",
+    path: "/Tab1",
+  },
+  {
+    tabName: "Tab 2",
+    path: "/Tab2",
+  },
+];
--- src/modules/NavBar/NavBar.js
+++ src/modules/NavBar/NavBar.js
@@ -1,6 +1,35 @@
+import React from "react";
 import "./NavBar.css";
+
+import { Box, Typography } from "@mui/material";
+import { useNavigate, createSearchParams } from "react-router-dom";
+import { NavTabs } from "../../data/NavTabs";
+
 function NavBar() {
-  return <div>NavBar</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <Box className="Navbar">
+        {NavTabs.map((tab) => (
+          <Typography
+            className="Navbar__Tab"
+            variant="subtitle1"
+            sx={{ cursor: "pointer" }}
+            onClick={() => {
+              navigate({
+                pathname: tab.path,
+                // search: `?${createSearchParams({
+                //   cno: chapter.chapterNum,
+                // })}`,
+              });
+            }}
+          >
+            {tab.tabName}
+          </Typography>
+        ))}
+      </Box>
+    </>
+  );
 }
 
 export default NavBar;
